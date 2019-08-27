83d164cd68a62a634bb8f011a748ea01c542b6f6
Alexander Milligan <alexander.ryan.milligan@gmail.com>
2019-08-26 20:21:25 -0500

    Create hello.htm

---
+++ src/viewSource.js
@@ -0,0 +1,11 @@
+function viewSource(s){;
+    var source = "<html>";
+    source += document.getElementsByTagName('html')[0].innerHTML;
+    source += "</html>";
+    source = source.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    source = "<pre>"+source+"</pre>";
+    sourceWindow = window.open(s,'Source of ' + s,'height=800,width=800,scrollbars=1,resizable=1');
+    sourceWindow.document.write(source);
+    sourceWindow.document.close(); 
+    if(window.focus) sourceWindow.focus();
+}
